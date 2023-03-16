d317b7d6e31a37067d0dbadb8cb8bb8afca0862d
Ersin Biltekin <ersin.biltekin.57@gmail.com>
2023-03-17 01:38:42 +0300

    Initial Test Commit

---
+++ src/application.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const application = express();
+
+const port = process.env.PORT || 3000;
+
+application
+	.get("/", (req, res) => { res.send({ message: "Hello, World!" }); })
+	.get("/random", (req, res) => { res.send({ number: Math.floor(Math.random() * 100) }); });
+
+application.listen(port, () => { console.log(`Application listening on port ${port}`); });
