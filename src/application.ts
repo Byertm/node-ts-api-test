5673b3cde59ce6a87a06d7d1068b5091aaffa99a
Ersin Biltekin <ersin.biltekin.57@gmail.com>
2023-03-18 05:28:35 +0300

    Application Type Support Added, Serverles And Node Fetch Plugin Added, TsConfig Edited

--- src/application.ts
+++ src/application.ts
@@ -1,11 +1,21 @@
-import express from "express";
+import express from 'express';
+import type { Request, Response, NextFunction } from 'express';
+import serverless from 'serverless-http';
 
 const application = express();
 
 const port = process.env.PORT || 3000;
 
 application
-	.get("/", (req, res) => { res.send({ message: "Hello, World!" }); })
-	.get("/random", (req, res) => { res.send({ number: Math.floor(Math.random() * 100) }); });
+	.get('/', (req: Request, res: Response) => {
+		res.send({ message: 'Hello, World!' });
+	})
+	.get('/random', (req: Request, res: Response) => {
+		res.send({ number: Math.floor(Math.random() * 100) });
+	});
 
-application.listen(port, () => { console.log(`Application listening on port ${port}`); });
+application.listen(port, () => {
+	console.log(`Application listening on port http://localhost:${port}`);
+});
+
+export const handler = serverless(application);
